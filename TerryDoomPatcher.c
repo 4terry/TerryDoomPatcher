2488a5105ee4e2d97b576cf1a29fe181c2b6f18d
4terry <pattern.wired@gmail.com>
2025-12-16 02:44:21 +0100

    dodane logo, naprawiony string, parsowanie zasobów

--- TerryDoomPatcher.c
+++ TerryDoomPatcher.c
@@ -1,20 +1,39 @@
 #define _CRT_SECURE_NO_WARNINGS //obejscie bledu fopen unsafe
 #include <stdio.h>
 #include <stdint.h> //int types
+#include <stdlib.h> //malloc
+#include <string.h>
 #include "wad.h"
+
+void logo();
+lumpInfo* new_lump();
 
 int main()
 {
-    printf("hello world!\n");
-
+    logo();
     wadHeader naglowek;
 
     FILE* doomFile = fopen("DOOM1.WAD", "rb");
     if (doomFile) {
         printf("wczytano plik DOOMa\n");
         fread(&naglowek, 4, 3, doomFile);
-        printf("typ pliku: %s\n", naglowek.wadType);
-        printf("ilosc lumps: %d\n", naglowek.numLumps);
+        printf("typ pliku: %.4s\n", naglowek.wadType);
+        printf("ilosc zasobow: %d (w bajtach: %d)\n", naglowek.numLumps, naglowek.numLumps*16);
+        printf("pointer do zasobow: 0x%X\n", naglowek.dirPtr);
+
+        lumpInfo* head = NULL;
+        lumpInfo* tmp = NULL;
+        fseek(doomFile, naglowek.dirPtr, SEEK_SET);
+
+        for (int i = 1; i < naglowek.numLumps; i++) {
+            tmp = new_lump();
+            fread(tmp, 16, 1, doomFile);
+            tmp->next = head;
+            head = tmp;
+            printf("nazwa zasobu: %.8s\n", head->lumpName);
+            printf("wielkosc zasobu w bajtach: %d\n", head->lumpSizeBytes);
+        }
+
     }
     else {
         printf("error, nie udalo sie uzyskac dostepu do pliku");
@@ -22,4 +41,20 @@ int main()
     }
 
     return 0;
+}
+
+void logo() {
+    printf("\n");
+    printf("___________                         ________                       __________         __         .__                  \n");
+    printf("\\__    ___/_________________ ___.__.\\______ \\   ____   ____   _____\\______   \\_____ _/  |_  ____ |  |__   ___________ \n");
+    printf("  |    |_/ __ \\_  __ \\_  __ <   |  | |    |  \\ /  _ \\ /  _ \\ /     \\|     ___/\\__  \\\\   __\\/ ___\\|  |  \\_/ __ \\_  __ \\\n");
+    printf("  |    |\\  ___/|  | \\/|  | \\/\\___  | |    `   (  <_> |  <_> )  Y Y  \\    |     / __ \\|  | \\  \\___|   Y  \\  ___/|  | \\/\n");
+    printf("  |____| \\___  >__|   |__|   / ____|/_______  /\\____/ \\____/|__|_|  /____|    (____  /__|  \\___  >___|  /\\___  >__|   \n");
+    printf("             \\/              \\/             \\/                    \\/               \\/          \\/     \\/     \\/       \n");
+}
+
+lumpInfo* new_lump() {
+    lumpInfo* result = malloc(sizeof(lumpInfo));
+    result->next = NULL;
+    return result;
 }
--- wad.h
+++ wad.h
@@ -7,3 +7,11 @@ typedef struct {
     int32_t numLumps;   // 0x04
     int32_t dirPtr;     // 0x08
 } wadHeader;
+
+typedef struct {
+    int32_t lumpPtr;
+    int32_t lumpSizeBytes;
+    char lumpName[8];
+
+    struct lumpInfo* next;
+} lumpInfo;
